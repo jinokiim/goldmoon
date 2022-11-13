3ae8175f0f965c347df89f96f07c67961175fd15
Jinho Kim <wlsghrla94@gmail.com>
2022-11-14 00:59:45 +0900

    변경 확인

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import { NextPage } from 'next';
 
 const IndexPage: NextPage = function () {
-  return <>Hello world</>;
+  return (
+    <>
+      <h1>하이 골드문</h1>
+    </>
+  );
 };
 
 export default IndexPage;
