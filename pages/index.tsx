1c9f26998e54f2a84ee058967919cccf82baa861
Jinho Kim <wlsghrla94@gmail.com>
2022-12-03 14:28:39 +0900

    [feat] layout 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
 import { NextPage } from 'next';
 import Button from '@mui/material/Button';
 
+import LayoutMain from 'layouts/main';
+import { Box } from '@mui/material';
+import useSettings from '@/hooks/useSettings';
+
 const IndexPage: NextPage = function () {
+  const { headerHeight } = useSettings();
+
   return (
-    <>
-      <h1>하이 골드문</h1>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </>
+    <LayoutMain
+      hasNav
+      styles={{
+        header: {
+          height: 56,
+          justifyContent: 'center'
+        }
+      }}
+      header={<h1>header check</h1>}
+      content={
+        <Box sx={{ px: 2.5, pb: 3, pt: headerHeight === 0 ? 7 : 2.5 }}>
+          <h1>하이s</h1>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+          <Button variant="outlined">이동버튼</Button>
+        </Box>
+      }
+    />
   );
 };
 
