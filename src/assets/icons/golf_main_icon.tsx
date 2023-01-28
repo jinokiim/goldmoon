1badf7cbd1ce7e6d0bbd40ff14443c0e5035ea64
Jinho Kim <wlsghrla94@gmail.com>
2023-01-28 17:16:17 +0900

    [fix] 불필요 선언문 삭제

--- src/assets/icons/golf_main_icon.tsx
+++ src/assets/icons/golf_main_icon.tsx
@@ -1,4 +1,4 @@
-import { Box, BoxProps } from '@mui/material';
+import { Box } from '@mui/material';
 // import { COLORS } from '../../theme/palette';
 // ----------------------------------------------------------------------
 
