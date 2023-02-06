2a6aab22e3c1e7ee8a8476d91b5f4dcbbd477aea
Jinho Kim <wlsghrla94@gmail.com>
2023-02-06 23:03:59 +0900

    add attendance page

---
+++ src/pages/main/attendance/index.tsx
@@ -0,0 +1,45 @@
+// import { NextPage } from 'next';
+
+// import Button from '@mui/material/Button';
+import Layout from '@/src/layouts/main';
+import { Box } from '@mui/material';
+// import useSettings from '@/src/hooks/useSettings';
+
+import { useEffect, useState } from 'react';
+
+import _ from 'lodash';
+
+// ----------------------------------------------------------------------
+
+const IndexPage = function () {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    mounted && (
+      <Layout
+        header={<></>}
+        content={
+          <Box
+            sx={{
+              pt: 7,
+              px: 2,
+              height: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center'
+            }}
+          >
+            <Box sx={{ height: '100%' }}>
+              <h1>출석, 회비납부 페이지</h1>
+            </Box>
+          </Box>
+        }
+      />
+    )
+  );
+};
+
+export default IndexPage;
