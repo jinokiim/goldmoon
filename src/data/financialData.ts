1f54cf00b333be53321932dfef23836a1dc911f8
Jinho Kim <wlsghrla94@gmail.com>
2023-02-11 16:49:51 +0900

    update financial data

---
+++ src/data/financialData.ts
@@ -0,0 +1,168 @@
+/**
+{
+  id: '',
+  year: '2023',
+  month: '',
+  history: [
+    {
+      date: '',
+      condition: '',
+      amount: '',
+      detail: '',
+      location: ''
+    }
+  ]
+}
+ */
+
+// ------------------------------------------------------------------------
+
+export const financial = [
+  {
+    id: '1',
+    year: '2022',
+    month: '11',
+    history: [
+      {
+        date: '5',
+        condition: 'income',
+        amount: '1704',
+        detail: '기부금',
+        location: ''
+      },
+      {
+        date: '7',
+        condition: 'income',
+        amount: '695000',
+        detail: '회비 입금',
+        location: ''
+      },
+      {
+        date: '11',
+        condition: 'spending',
+        amount: '60000',
+        detail: '11월 정모1',
+        location: ''
+      },
+      {
+        date: '25',
+        condition: 'spending',
+        amount: '100000',
+        detail: '11월 정모2',
+        location: ''
+      },
+      {
+        date: '26',
+        condition: 'income',
+        amount: '50',
+        detail: '이자',
+        location: ''
+      }
+    ]
+  },
+  {
+    id: '2',
+    year: '2022',
+    month: '12',
+    history: [
+      {
+        date: '7',
+        condition: 'income',
+        amount: '240000',
+        detail: '회비 입금',
+        location: ''
+      },
+      {
+        date: '12',
+        condition: 'spending',
+        amount: '105000',
+        detail: '12월 정모1',
+        location: ''
+      },
+      {
+        date: '24',
+        condition: 'income',
+        amount: '498',
+        detail: '이자',
+        location: ''
+      },
+      {
+        date: '30',
+        condition: 'spending',
+        amount: '45000',
+        detail: '12월 정모2',
+        location: ''
+      }
+    ]
+  },
+  {
+    id: '3',
+    year: '2023',
+    month: '01',
+    history: [
+      {
+        date: '7',
+        condition: 'income',
+        amount: '550000',
+        detail: '회비 입금',
+        location: ''
+      },
+      {
+        date: '14',
+        condition: 'spending',
+        amount: '189000',
+        detail: '신년회 경품',
+        location: '롯데아울렛 광교점'
+      },
+      {
+        date: '14',
+        condition: 'spending',
+        amount: '10000',
+        detail: '신년회 경품',
+        location: '광교 골프존 주식회사'
+      },
+      {
+        date: '15',
+        condition: 'spending',
+        amount: '145000',
+        detail: '신년회',
+        location: '광교 돈블랑'
+      },
+      {
+        date: '28',
+        condition: 'income',
+        amount: '74',
+        detail: '이자',
+        location: ''
+      },
+      {
+        date: '30',
+        condition: 'spending',
+        amount: '100000',
+        detail: '1월 정모2',
+        location: ''
+      }
+    ]
+  },
+  {
+    id: '4',
+    year: '2023',
+    month: '02',
+    history: [
+      {
+        date: '7',
+        condition: 'income',
+        amount: '380000',
+        detail: '회비 입금',
+        location: ''
+      },
+      {
+        date: '10',
+        condition: 'income',
+        amount: '796',
+        detail: '카카오뱅크 캐시백지급',
+        location: ''
+      }
+    ]
+  }
+];
