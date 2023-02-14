de4d1ee64ec557df0e093ec0a0ab71a6837f1a62
Jinho Kim <wlsghrla94@gmail.com>
2023-02-14 23:05:13 +0900

    add history page

---
+++ src/pages/main/financial/history/index.tsx
@@ -0,0 +1,134 @@
+// import { NextPage } from 'next';
+
+// import Button from '@mui/material/Button';
+import Layout from '@/src/layouts/main';
+import { Box, Divider, Stack, Typography } from '@mui/material';
+// import useSettings from '@/src/hooks/useSettings';
+
+import { useEffect, useState } from 'react';
+
+import _ from 'lodash';
+import { useRouter } from 'next/router';
+import AppHeader from '@/src/components/Header';
+import { COLORS } from '@/src/theme/palette';
+import { financialHistory } from '@/src/data/financialData';
+
+// ----------------------------------------------------------------------
+
+const IndexPage = function () {
+  const router = useRouter();
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    mounted && (
+      <Layout
+        header={
+          <>
+            <AppHeader onPrev={() => router.push('/main')} onPrevText={'회비내역 히스토리'} />
+          </>
+        }
+        content={
+          <Box
+            sx={{
+              pt: 7,
+              px: 2,
+              height: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center'
+            }}
+          >
+            <Box sx={{ height: '100%' }}>
+              <Box sx={{ px: 0.5 }}>
+                {/* <PaymentList
+                item={{
+                  description: item.merchant_name,
+                  value: item.approved_amt,
+                  date: item.approved_dtime,
+                  descriptionMore: item.memo,
+                  currencyCode: item.currency_code,
+                  canceled: item.status === '02'
+                }}
+              /> */}
+
+                {financialHistory?.map((item, index) => (
+                  <>
+                    <Box key={index}>
+                      <Stack direction="row" justifyContent="space-between">
+                        <Typography
+                          variant="body1"
+                          sx={{
+                            fontWeight: 400,
+                            maxWidth: 200,
+                            overflow: 'hidden',
+                            whiteSpace: 'nowrap'
+                          }}
+                          textOverflow="ellipsis"
+                        >
+                          {item.detail}
+                        </Typography>
+                        <Typography
+                          variant="body1"
+                          sx={{
+                            fontWeight: item.condition === 'income' ? 600 : 400,
+                            color:
+                              item.condition === 'income' ? COLORS.secondary500 : COLORS.text900
+                          }}
+                        >
+                          {item.condition === 'spending' && '-'}
+                          {Number(item.amount).toLocaleString()} 원
+                        </Typography>
+                      </Stack>
+                      <Typography
+                        variant="body2"
+                        sx={{ color: COLORS.text600, mt: '2px', mb: 1.5 }}
+                      >
+                        {item.year}년 {item.month}월 {item.date}일
+                      </Typography>
+                      <Divider sx={{ my: 2 }} />
+                    </Box>
+                  </>
+                ))}
+              </Box>
+            </Box>
+          </Box>
+        }
+      />
+    )
+  );
+};
+
+export default IndexPage;
+
+export function PaymentList() {
+  return (
+    <>
+      <Stack direction="row" justifyContent="space-between">
+        <Typography
+          variant="body1"
+          sx={{
+            fontWeight: 400,
+            maxWidth: 200,
+            overflow: 'hidden',
+            whiteSpace: 'nowrap'
+          }}
+          textOverflow="ellipsis"
+        >
+          {'가게 정보 '}
+        </Typography>
+        <Typography variant="body1" sx={{ fontWeight: 400 }}>
+          8972403 원
+        </Typography>
+      </Stack>
+      <Typography variant="body2" sx={{ color: COLORS.text600, mt: '2px', mb: 1.5 }}>
+        2023년 00월 00일
+      </Typography>
+      <Divider sx={{ my: 2 }} />
+    </>
+  );
+}
