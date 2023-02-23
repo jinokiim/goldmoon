0f02f89bc504174c203cdf83c43d53603ac59324
Jinho KIM <88222461+jinokiim@users.noreply.github.com>
2023-02-23 15:35:56 +0900

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,24 +2,35 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import ThemeConfig from '../theme';
 import ThemePrimaryColor from '../components/ThemePrimaryColor';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const MyApp = (props: AppProps) => {
   const { Component, pageProps } = props;
 
+  const [isScrolling, setIsScrolling] = useState(false);
   const ref = useRef(null);
 
   useEffect(() => {
     const preventZoom = (event: { touches: any[]; preventDefault: () => void }) => {
-      if (event.touches.length > 1) {
-        event.preventDefault();
-      } else {
-        const touch = event.touches[0];
-        const distance = Math.sqrt(touch.clientX ** 2 + touch.clientY ** 2);
-        if (distance < 150) {
+      if (!isScrolling) {
+        if (event.touches.length > 1) {
           event.preventDefault();
+        } else {
+          const touch = event.touches[0];
+          const distance = Math.sqrt(touch.clientX ** 2 + touch.clientY ** 2);
+          if (distance < 150) {
+            event.preventDefault();
+          }
         }
       }
+    };
+
+    const startScrolling = () => {
+      setIsScrolling(true);
+    };
+
+    const stopScrolling = () => {
+      setIsScrolling(false);
     };
 
     const element = ref.current;
@@ -27,15 +38,23 @@ const MyApp = (props: AppProps) => {
     if (element) {
       /* @ts-ignore */
       element.addEventListener('touchmove', preventZoom, { passive: false });
+      /* @ts-ignore */
+      element.addEventListener('touchstart', startScrolling, { passive: false });
+      /* @ts-ignore */
+      element.addEventListener('touchend', stopScrolling, { passive: false });
     }
 
     return () => {
       if (element) {
         /* @ts-ignore */
         element.removeEventListener('touchmove', preventZoom);
+        /* @ts-ignore */
+        element.removeEventListener('touchstart', startScrolling);
+        /* @ts-ignore */
+        element.removeEventListener('touchend', stopScrolling);
       }
     };
-  }, []);
+  }, [isScrolling]);
 
   return (
     <>
