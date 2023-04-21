5ec23876f0d7c5d978166b309325956066046f7e
Jinho KIM <88222461+jinokiim@users.noreply.github.com>
2023-04-21 16:07:44 +0900

    update financial data

--- src/data/financialData.ts
+++ src/data/financialData.ts
@@ -1,263 +1,286 @@
 /**
-{
-  id: '',
-  year: '2023',
-  month: '',
-  history: [
-    {
-      date: '',
-      condition: '',
-      amount: '',
-      detail: '',
-      location: ''
-    }
-  ]
-}
+{id: '',year: '2023',month: '',history: [{date: '',condition: '',amount: '',detail: '',location: ''}]},
  */
 
 // ------------------------------------------------------------------------
-export const financialLastUpdated = "23년 03월 13일 14시";
+export const financialLastUpdated = '23년 04월 21일 16시';
 export const financialNow = {
-  total: "1119190",
-  income: "500068",
-  spending: "300000",
+  total: '1399284',
+  income: '390000',
+  spending: '40000'
 };
 
 export const financialHistory = [
   {
-    id: "22",
-    year: "2023",
-    month: "03",
+    id: '24',
+    year: '2023',
+    month: '04',
     history: [
       {
-        date: "11",
-        condition: "spending",
-        amount: "90000",
-        detail: "3월 주말정모 새대전",
-        location: "골프존파크 수지24",
-      },
-    ],
+        date: '16',
+        condition: 'spending',
+        amount: '40000',
+        detail: '4월 주말정모',
+        location: '골프존 파크 법조타운점'
+      }
+    ]
   },
   {
-    id: "21",
-    year: "2023",
-    month: "03",
+    id: '23',
+    year: '2023',
+    month: '04',
+    history: [
+      { date: '07', condition: 'income', amount: '390000', detail: '4월 회비금 입금', location: '' }
+    ]
+  },
+  {
+    id: '22',
+    year: '2023',
+    month: '03',
     history: [
       {
-        date: "10",
-        condition: "spending",
-        amount: "210000",
-        detail: "새대전 상품 구매",
-        location: "",
-      },
-    ],
+        date: '25',
+        condition: 'spending',
+        amount: '70000',
+        detail: '3월 평일정모',
+        location: '골프존파크 광교레이크'
+      }
+    ]
   },
   {
-    id: "20",
-    year: "2023",
-    month: "03",
+    id: '22',
+    year: '2023',
+    month: '03',
     history: [
       {
-        date: "07",
-        condition: "income",
-        amount: "500000",
-        detail: "3월 회비 금입금",
-        location: "",
-      },
-    ],
+        date: '25',
+        condition: 'spending',
+        amount: '90000',
+        detail: '3월 주말정모 새대전',
+        location: '골프존파크 수지24'
+      }
+    ]
   },
   {
-    id: "19",
-    year: "2023",
-    month: "02",
+    id: '21',
+    year: '2023',
+    month: '03',
     history: [
       {
-        date: "25",
-        condition: "income",
-        amount: "68",
-        detail: "입출금 통장 이자",
-        location: "",
-      },
-    ],
+        date: '10',
+        condition: 'spending',
+        amount: '210000',
+        detail: '새대전 상품 구매',
+        location: ''
+      }
+    ]
   },
   {
-    id: "18",
-    year: "2023",
-    month: "02",
-    date: "24",
-    condition: "spending",
-    amount: "200000",
-    detail: "2월 평일정모",
-    location: "골프존파크 수지24",
+    id: '20',
+    year: '2023',
+    month: '03',
+    history: [
+      {
+        date: '07',
+        condition: 'income',
+        amount: '500000',
+        detail: '3월 회비금입금',
+        location: ''
+      }
+    ]
   },
   {
-    id: "17",
-    year: "2023",
-    month: "02",
-    date: "10",
-    condition: "income",
-    amount: "796",
-    detail: "카카오뱅크 캐시백지급",
-    location: "",
+    id: '19',
+    year: '2023',
+    month: '02',
+    history: [
+      {
+        date: '25',
+        condition: 'income',
+        amount: '68',
+        detail: '입출금 통장 이자',
+        location: ''
+      }
+    ]
   },
   {
-    id: "16",
-    year: "2023",
-    month: "02",
-    date: "7",
-    condition: "income",
-    amount: "380000",
-    detail: "회비 입금",
-    location: "",
+    id: '18',
+    year: '2023',
+    month: '02',
+    date: '24',
+    condition: 'spending',
+    amount: '200000',
+    detail: '2월 평일정모',
+    location: '골프존파크 수지24'
   },
   {
-    id: "15",
-    year: "2023",
-    month: "01",
-    date: "30",
-    condition: "spending",
-    amount: "100000",
-    detail: "1월 정모2",
-    location: "",
+    id: '17',
+    year: '2023',
+    month: '02',
+    date: '10',
+    condition: 'income',
+    amount: '796',
+    detail: '카카오뱅크 캐시백지급',
+    location: ''
   },
   {
-    id: "14",
-    year: "2023",
-    month: "01",
-    date: "28",
-    condition: "income",
-    amount: "74",
-    detail: "이자",
-    location: "",
+    id: '16',
+    year: '2023',
+    month: '02',
+    date: '7',
+    condition: 'income',
+    amount: '380000',
+    detail: '회비 입금',
+    location: ''
   },
   {
-    id: "13",
-    year: "2023",
-    month: "01",
-    date: "15",
-    condition: "spending",
-    amount: "145000",
-    detail: "신년회",
-    location: "광교 돈블랑",
+    id: '15',
+    year: '2023',
+    month: '01',
+    date: '30',
+    condition: 'spending',
+    amount: '100000',
+    detail: '1월 정모2',
+    location: ''
   },
   {
-    id: "12",
-    year: "2023",
-    month: "01",
-    date: "14",
-    condition: "spending",
-    amount: "10000",
-    detail: "신년회 경품",
-    location: "광교 골프존 주식회사",
+    id: '14',
+    year: '2023',
+    month: '01',
+    date: '28',
+    condition: 'income',
+    amount: '74',
+    detail: '이자',
+    location: ''
   },
   {
-    id: "11",
-    year: "2023",
-    month: "01",
-    date: "14",
-    condition: "spending",
-    amount: "189000",
-    detail: "신년회 경품",
-    location: "롯데아울렛 광교점",
+    id: '13',
+    year: '2023',
+    month: '01',
+    date: '15',
+    condition: 'spending',
+    amount: '145000',
+    detail: '신년회',
+    location: '광교 돈블랑'
   },
   {
-    id: "10",
-    year: "2023",
-    month: "01",
-    date: "7",
-    condition: "income",
-    amount: "550000",
-    detail: "회비 입금",
-    location: "",
+    id: '12',
+    year: '2023',
+    month: '01',
+    date: '14',
+    condition: 'spending',
+    amount: '10000',
+    detail: '신년회 경품',
+    location: '광교 골프존 주식회사'
   },
   {
-    id: "9",
-    year: "2022",
-    month: "12",
-    date: "30",
-    condition: "spending",
-    amount: "45000",
-    detail: "12월 정모2",
-    location: "",
+    id: '11',
+    year: '2023',
+    month: '01',
+    date: '14',
+    condition: 'spending',
+    amount: '189000',
+    detail: '신년회 경품',
+    location: '롯데아울렛 광교점'
   },
   {
-    id: "8",
-    year: "2022",
-    month: "12",
-    date: "24",
-    condition: "income",
-    amount: "498",
-    detail: "이자",
-    location: "",
+    id: '10',
+    year: '2023',
+    month: '01',
+    date: '7',
+    condition: 'income',
+    amount: '550000',
+    detail: '회비 입금',
+    location: ''
   },
   {
-    id: "7",
-    year: "2022",
-    month: "12",
-    date: "12",
-    condition: "spending",
-    amount: "105000",
-    detail: "12월 정모1",
-    location: "",
+    id: '9',
+    year: '2022',
+    month: '12',
+    date: '30',
+    condition: 'spending',
+    amount: '45000',
+    detail: '12월 정모2',
+    location: ''
   },
   {
-    id: "6",
-    year: "2022",
-    month: "12",
-    date: "7",
-    condition: "income",
-    amount: "240000",
-    detail: "회비 입금",
-    location: "",
+    id: '8',
+    year: '2022',
+    month: '12',
+    date: '24',
+    condition: 'income',
+    amount: '498',
+    detail: '이자',
+    location: ''
   },
   {
-    id: "5",
-    year: "2022",
-    month: "11",
-    date: "26",
-    condition: "income",
-    amount: "50",
-    detail: "이자",
-    location: "",
+    id: '7',
+    year: '2022',
+    month: '12',
+    date: '12',
+    condition: 'spending',
+    amount: '105000',
+    detail: '12월 정모1',
+    location: ''
   },
   {
-    id: "4",
-    year: "2022",
-    month: "11",
-    date: "25",
-    condition: "spending",
-    amount: "100000",
-    detail: "11월 정모2",
-    location: "",
+    id: '6',
+    year: '2022',
+    month: '12',
+    date: '7',
+    condition: 'income',
+    amount: '240000',
+    detail: '회비 입금',
+    location: ''
   },
   {
-    id: "3",
-    year: "2022",
-    month: "11",
-    date: "11",
-    condition: "spending",
-    amount: "60000",
-    detail: "11월 정모1",
-    location: "",
+    id: '5',
+    year: '2022',
+    month: '11',
+    date: '26',
+    condition: 'income',
+    amount: '50',
+    detail: '이자',
+    location: ''
   },
   {
-    id: "2",
-    year: "2022",
-    month: "11",
-    date: "7",
-    condition: "income",
-    amount: "695000",
-    detail: "회비 입금",
-    location: "",
+    id: '4',
+    year: '2022',
+    month: '11',
+    date: '25',
+    condition: 'spending',
+    amount: '100000',
+    detail: '11월 정모2',
+    location: ''
   },
   {
-    id: "1",
-    year: "2022",
-    month: "11",
-    date: "5",
-    condition: "income",
-    amount: "1704",
-    detail: "기부금",
-    location: "",
+    id: '3',
+    year: '2022',
+    month: '11',
+    date: '11',
+    condition: 'spending',
+    amount: '60000',
+    detail: '11월 정모1',
+    location: ''
   },
+  {
+    id: '2',
+    year: '2022',
+    month: '11',
+    date: '7',
+    condition: 'income',
+    amount: '695000',
+    detail: '회비 입금',
+    location: ''
+  },
+  {
+    id: '1',
+    year: '2022',
+    month: '11',
+    date: '5',
+    condition: 'income',
+    amount: '1704',
+    detail: '기부금',
+    location: ''
+  }
 ];
